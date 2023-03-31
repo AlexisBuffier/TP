1dbf98b103b59c684cbb97ec6db9a476bcbc96ae
Alexis <alexis.buffier0100@orange.fr>
2023-03-31 14:17:01 +0200

    exo 1

---
+++ Exercice 1/app.js
@@ -0,0 +1,8 @@
+var btnHide = document.getElementById('hide');
+
+btnHide.addEventListener('click', function()
+{
+    var div = document.getElementById('div');
+
+    div.style.display = "none";
+});
